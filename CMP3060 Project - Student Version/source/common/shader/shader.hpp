031f9e4c6b995475820a33c96d31a5d006a66977
norhanreda <norhan.ahmed01@eng-st.cu.edu.eg>
2023-03-10 20:36:10 +0200

    finish

--- CMP3060 Project - Student Version/source/common/shader/shader.hpp
+++ CMP3060 Project - Student Version/source/common/shader/shader.hpp
@@ -59,7 +59,6 @@ namespace our {
 
         void set(const std::string &uniform, glm::vec2 value) {
             //TODO: (Req 1) Send the given 2D vector value to the given uniform
-            glUniform2f(getUniformLocation(uniform), value.x, value.y);
         }
  
         void set(const std::string &uniform, glm::vec3 value) {
