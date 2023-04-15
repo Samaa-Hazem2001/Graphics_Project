5ccb73959b593938f6575719b7da770d44049c2e
norhanreda <norhan.ahmed01@eng-st.cu.edu.eg>
2023-04-15 18:31:34 +0200

    adding  req 11 sol

--- source/common/texture/texture-utils.cpp
+++ source/common/texture/texture-utils.cpp
@@ -8,6 +8,12 @@
 our::Texture2D* our::texture_utils::empty(GLenum format, glm::ivec2 size){
     our::Texture2D* texture = new our::Texture2D();
     //TODO: (Req 11) Finish this function to create an empty texture with the given size and format
+     texture->bind();
+    if(format == GL_RGBA8) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, size[0], size[1], 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+    } else if(format == GL_DEPTH_COMPONENT24)
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT24, size[0], size[1], 0, GL_DEPTH_COMPONENT, GL_UNSIGNED_BYTE, NULL);
+   
 
     return texture;
 }
