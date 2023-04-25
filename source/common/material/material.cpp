b08c5c30b9ab70600c529f22659ac581e88d9e92
hodagamal <hoda.ismail00@eng-st.cu.edu.eg>
2023-04-25 23:48:43 +0200

    solve bug in textured material

--- source/common/material/material.cpp
+++ source/common/material/material.cpp
@@ -8,8 +8,8 @@ namespace our {
     // This function should setup the pipeline state and set the shader to be used
     void Material::setup() const {
         //TODO: (Req 7) Write this function
-         this->pipelineState.setup();
-         this->shader->use();
+        this->pipelineState.setup();
+        this->shader->use();
     }
 
     // This function read the material data from a json object
@@ -27,7 +27,7 @@ namespace our {
     // set the "tint" uniform to the value in the member variable tint 
     void TintedMaterial::setup() const {
         //TODO: (Req 7) Write this function
-         Material::setup();
+        Material::setup();
         shader->set("tint",tint);
     }
 
@@ -43,11 +43,14 @@ namespace our {
     // Then it should bind the texture and sampler to a texture unit and send the unit number to the uniform variable "tex" 
     void TexturedMaterial::setup() const {
         //TODO: (Req 7) Write this function
-       TintedMaterial::setup();
+        TintedMaterial::setup();
         shader->set("alphaThreshold",alphaThreshold);
-        glActiveTexture(GL_TEXTURE0); //we send it unit 0 
-        texture->bind();
-        sampler->bind(0);
+        
+        if(texture != NULL && sampler !=NULL){
+            glActiveTexture(GL_TEXTURE0); //we send it unit 0 
+            texture->bind();
+            sampler->bind(0);
+        }
         shader->set("tex",0);
     }
 
