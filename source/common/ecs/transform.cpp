7563894be10f6ad76ae5eb07938282e7bfad4898
Samaa-Hazem2001 <samaaabdellatif957@gmail.com>
2023-04-28 12:35:42 +0300

    add req 3 documentation

--- source/common/ecs/transform.cpp
+++ source/common/ecs/transform.cpp
@@ -10,9 +10,19 @@ namespace our {
     // HINT: to convert euler angles to a rotation matrix, you can use glm::yawPitchRoll
     glm::mat4 Transform::toMat4() const {
         //TODO: (Req 3) Write this function
+
+        //here we use the concept of "Translation Rotation Scaling (TRS)"
+        //as Scaling,Rotation,Translation can also be stored in a 3D vector 
+        //then we use them with scale,yawPitchRoll,translate by sending them with unit matrix 
+        //so we will be apple to avoid the human errors in assigning matrices values
+
         glm::mat4 matrixScale = glm::scale(glm::mat4(1.0f), scale);
+        //for more about yawPitchRoll animation to specify wi=hich one to which axis see this:
+        // https://howthingsfly.si.edu/flight-dynamics/roll-pitch-and-yaw
         glm::mat4 matrixRotation = glm::yawPitchRoll(rotation.y,rotation.x,rotation.z);
         glm::mat4 matrixTranslation = glm::translate(glm::mat4(1.0f), position);
+
+        //with order of TRS 
         return matrixTranslation * matrixRotation * matrixScale ;
     }
 
